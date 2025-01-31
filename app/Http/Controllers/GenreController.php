eeb3d0d2567cc7882775b7d8daae83e30e79b71d
DESKTOP-78N5INS\Meddie <postvoormathijs@gmail.com>
2025-01-31 22:47:20 +0100

    setup genres & tests

---
+++ app/Http/Controllers/GenreController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\GenreCollection;
+use App\Models\Genre;
+use Illuminate\Http\Request;
+
+class GenreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $genres = Genre::paginate(10);
+
+        return new GenreCollection($genres);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,3 +2,6 @@
 
 // movies resource
 Route::resource('movies', \App\Http\Controllers\MoviesController::class)->only(['index']);
+
+// genres resource
+Route::resource('genres', \App\Http\Controllers\GenreController::class)->only(['index']);
