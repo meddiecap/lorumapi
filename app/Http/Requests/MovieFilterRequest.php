590c65c723c0a35d12a3418c4cc04c6209ce4901
DESKTOP-78N5INS\Meddie <postvoormathijs@gmail.com>
2025-02-02 21:18:26 +0100

    min/max rating validation fix

--- app/Http/Requests/MovieFilterRequest.php
+++ app/Http/Requests/MovieFilterRequest.php
@@ -16,8 +16,30 @@ class MovieFilterRequest extends FormRequest
         return [
             'release_year' => 'integer',
             'genre' => 'string',
-            'min_rating' => 'numeric|lt:max_rating|nullable|min:0',
-            'max_rating' => 'numeric|gt:min_rating|nullable|max:10',
+            'min_rating' => [
+                'numeric',
+                'nullable',
+                'min:0',
+                'max:10',
+                function ($attribute, $value, $fail) {
+                    // Custom validation rule to ensure min_rating is less than or equal to max_rating if both are provided
+                    if ($this->input('max_rating') && $value > $this->input('max_rating')) {
+                        $fail('The min_rating must be less than or equal to the max_rating.');
+                    }
+                },
+            ],
+            'max_rating' => [
+                'numeric',
+                'nullable',
+                'min:0',
+                'max:10',
+                function ($attribute, $value, $fail) {
+                    // Custom validation rule to ensure max_rating is greater than or equal to min_rating if both are provided
+                    if ($this->input('min_rating') && $value < $this->input('min_rating')) {
+                        $fail('The max_rating must be greater than or equal to the min_rating.');
+                    }
+                },
+            ],
         ];
     }
 }
